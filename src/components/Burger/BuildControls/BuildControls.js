99002bea09ad91094da435670099b11371eeea84
Axel <axosar@gmail.com>
2019-06-13 11:24:43 -0500

    Building the Modal and OrderSummary
    Modal and content with order summary

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -20,6 +20,7 @@ const buildControls = (props) => (
         remove={() => props.ingredientRemoved(ctrl.type)}
         disabled={props.disabled[ctrl.type]}  />
     ))}
+    <button className={classes.OrderButton}>ORDER NOW</button>
   </div>
 )
 
