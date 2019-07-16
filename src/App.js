ad951a54f16e15b56563b8e35483ef2a6e65f482
Axel <axosar@gmail.com>
2019-07-16 10:13:47 -0500

    Router Checkout
    Installing router-dom and checkout links

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
@@ -18,9 +19,11 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          {this.state.show ? <BurgerBuilder /> : null}
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" exact component={BurgerBuilder} />
+          </Switch>
         </Layout>
-        <Checkout />
       </div>
     );
   }
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,7 +4,7 @@ import NavigaItem from './NavigationItem/NavigationItem';
 
 const navigationItems = ()=>(
   <ul className={classes.NavigationItems}>
-    <NavigaItem link="/" active>BURGER BUILDER</NavigaItem>
+    <NavigaItem link="/" active>BURGER-BUILDER</NavigaItem>
     <NavigaItem link="/">CHECKOUT</NavigaItem>
   </ul>
 );
