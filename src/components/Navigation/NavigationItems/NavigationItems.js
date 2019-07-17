81738b3f3465f21f5f3d89ad98f435971c614f58
Axel <axosar@gmail.com>
2019-07-17 16:21:16 -0500

    Adding Navigation to Orders
    Routing to navigation Orders, setting styles for active.

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,8 +4,8 @@ import NavigaItem from './NavigationItem/NavigationItem';
 
 const navigationItems = ()=>(
   <ul className={classes.NavigationItems}>
-    <NavigaItem link="/" active>BURGER-BUILDER</NavigaItem>
-    <NavigaItem link="/">CHECKOUT</NavigaItem>
+    <NavigaItem link="/" active>Burger Builder</NavigaItem>
+    <NavigaItem link="/orders">Orders</NavigaItem>
   </ul>
 );
 
