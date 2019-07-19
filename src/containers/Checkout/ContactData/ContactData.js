fd22a598184a2bf5fefb62fb2d11fd778e6af72e
Axel <axosar@gmail.com>
2019-07-19 16:30:00 -0500

    Adding from MAP
    Maping through form elements.

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -85,12 +85,23 @@ class ContactData extends Component {
   }
 
   render(){
+    const formElementsArray = [];
+    for(let key in this.state.orderForm){
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key]
+      })
+    }
     let form = (
       <form action="">
-        <Input elementType='...' elementConfig='...' value='...' />
-        <Input inputtype="input" type="text" name="email" placeholder="Your Email" />
-        <Input inputtype="input" type="text" name="street" placeholder="Your Street" />
-        <Input inputtype="input" type="text" name="postalCode" placeholder="Your Postal Code" />
+        {formElementsArray.map(formElement => (
+          <Input 
+            key={formElement.id}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+          />
+        ))}
         <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
       </form>
     );
