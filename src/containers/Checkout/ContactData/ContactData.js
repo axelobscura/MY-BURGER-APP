4d3da8e4816060949d9fbb5b60885b250c4702f4
Axel <axosar@gmail.com>
2019-07-26 17:45:17 -0500

    Sending data to db
    sending data to db

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -67,10 +67,15 @@ class ContactData extends Component {
     e.preventDefault();
     //alert('You continue!!!');
     this.setState({loading:true});
+    const formData = {};
+    for(let formElementIdentifier in this.state.orderForm){
+      formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+
+    }
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.totalPrice,
-      
+      orderData: formData
     }
     axios.post('/orders.json', order)
       .then(response => {
@@ -106,7 +111,7 @@ class ContactData extends Component {
       })
     }
     let form = (
-      <form action="">
+      <form onSubmit={this.orderHandler}>
         {formElementsArray.map(formElement => (
           <Input 
             key={formElement.id}
@@ -116,7 +121,7 @@ class ContactData extends Component {
             changed={(event) => this.inputChangedHandler(event, formElement.id)}
           />
         ))}
-        <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+        <Button btnType="Success">ORDER</Button>
       </form>
     );
     if(this.state.loading){
